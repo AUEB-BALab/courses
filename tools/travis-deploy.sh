f899ad58d399beb8bcea4c07b8d4aebfd72c7095
stefanos1316 <stefanos1316@gmail.com>
2018-02-27 22:34:16 +0200

    Changes on encrypt_key

--- tools/travis-deploy.sh
+++ tools/travis-deploy.sh
@@ -67,19 +67,19 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
 
 if [ -n "$DEBUG" ] ; then
   set -x
 fi
 
-chmod 600 deploy_key
+chmod 600 ../deploy_key
 eval `ssh-agent -s`
 
 pwd
 ls -l 
 ls -l ../
-ssh-add deploy_key
+ssh-add ../deploy_key
 
 echo "I am not crushing here..."
 
